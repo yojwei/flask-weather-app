a9d96865d14e60eed382b1bffc8ef901c4ce84e6
Hsieh Chang Wei <yojwei125@gmail.com>
2025-12-31 21:38:46 +0800

    feat(auth): 更新註冊路由的文檔註解
    新增使用者註冊頁面的文檔註解，清晰描述 GET 和 POST 方法的功能。

--- flask_weather/auth/routes.py
+++ flask_weather/auth/routes.py
@@ -1,3 +1,9 @@
+"""
+使用者認證路由模組
+
+處理使用者註冊、登入和登出的相關路由和邏輯。
+"""
+
 from flask import render_template, redirect, url_for, flash
 from flask_weather import db
 from flask_weather.auth import auth_bp
@@ -7,11 +13,19 @@ from flask_weather.models import User
 
 @auth_bp.route("/register", methods=["GET", "POST"])
 def register():
+    """
+    使用者註冊頁面
+
+    GET: 返回註冊表單頁面
+    POST: 處理使用者提交的註冊資料
+    """
     form = RegistrationForm()
     if form.validate_on_submit():
+        # 建立新使用者物件並設定密碼
         user = User(username=form.username.data, email=form.email.data)
         user.set_password(form.password.data)
 
+        # 將新使用者儲存到資料庫
         db.session.add(user)
         db.session.commit()
 
